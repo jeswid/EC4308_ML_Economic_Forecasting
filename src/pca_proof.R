07608e9a5a81d427f419187274b9b72bf99118e1
yangshutingg <e0773572@u.nus.edu>
2024-10-19 19:26:24 +0800

    pca proof

---
+++ src/pca_proof.R
@@ -0,0 +1,13 @@
+check = readRDS("final_cleaned_data.RDS")
+# standardize the data
+df_temp = na.omit(check)
+# Convert factor or character columns to numeric (if appropriate)
+# df_temp <- df_temp %>% mutate_if(is.factor, as.numeric) %>% mutate_if(is.character, as.numeric)
+# Select only numeric columns
+numeric_df <- df_temp[, sapply(df_temp, is.numeric)]  # Remove the non-numeric column
+
+# principal component analysis 
+pca_result <- prcomp(numeric_df, center = TRUE, scale. = TRUE)
+summary(pca_result)  
+
+# because it takes 18 PCs to explain 80% of total variance, we omitted PCA in this analysis
