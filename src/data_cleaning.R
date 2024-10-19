8329ead54867202015f447b2ff0866ad05702984
yangshutingg <e0773572@u.nus.edu>
2024-10-19 10:03:57 +0800

    load hdm package

--- src/data_cleaning.R
+++ src/data_cleaning.R
@@ -1,9 +1,12 @@
-#remove all objects from memory
+# remove all objects from memory
 rm(list = ls())
 
 # load required packages
 library(readxl)
+library(hdm)
 
 # load datasets
 df = read_excel("data/ie_data.xls",sheet = 'Data',skip = 7)
 df_stock_returns = read_excel("data/Data2023.xlsx",sheet = 'Monthly')
+
+# logistic LASSO regression
