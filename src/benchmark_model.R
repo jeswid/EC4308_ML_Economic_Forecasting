b14260c0cbced0498ee4b9a4631da40b43e49c9e
yangshutingg <e0773572@u.nus.edu>
2024-10-24 22:49:07 +0800

    rename file

--- src/benchmark_model.R
+++ src/benchmark_model.R
@@ -1,4 +1,4 @@
-# Set seed for reproducibility
+# set seed for reproducibility
 set.seed(42)
 
 # Load the data
@@ -47,7 +47,7 @@ predicted_prob_logit_h1_df = as.data.frame(predicted_prob_logit_h1) %>%
 data = left_join(data,predicted_prob_logit_h1_df,by = "date")
 
 #logistic regression for h3
-logit_h3 <- glm(market_state ~  tms + lag3_ret + infl + lty, data = train_data, family = binomial(link = "logit"))
+logit_h3 <- glm(market_state ~  tms + lag4_ret + infl + lty, data = train_data, family = binomial(link = "logit"))
 # display summary of the model
 summary(logit_h3)
 # making predictions
@@ -58,7 +58,7 @@ predicted_prob_logit_h3_df = as.data.frame(predicted_prob_logit_h3) %>%
 data = left_join(data,predicted_prob_logit_h3_df,by = "date")
 
 #logistic regression for h6
-logit_h6 <- glm(market_state ~  tms + lag6_ret + infl + lty, data = train_data, family = binomial(link = "logit"))
+logit_h6 <- glm(market_state ~  tms + lag7_ret + infl + lty, data = train_data, family = binomial(link = "logit"))
 # display summary of the model
 summary(logit_h6)
 # making predictions
