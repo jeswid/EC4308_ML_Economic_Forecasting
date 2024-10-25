4bf1f5bd06e876f2f781eb550d242c10f24e9606
jeswid <jessicawidyawati@gmail.com>
2024-10-25 23:12:14 +0800

    updated data transformation

--- src/data_transformation.R
+++ src/data_transformation.R
@@ -13,11 +13,11 @@ adf.test(diff_log_series) # stationary
 df$dividend_price_ratio <- c(NA, diff(log(df$dividend_price_ratio)))
 
 
-# dividend yield
-adf.test(df$dividend_yield, alternative = "stationary") # non-stationary
-diff_log_series <- diff(log(df$dividend_yield))
+# SP price
+adf.test(df$price, alternative = "stationary") # non-stationary
+diff_log_series <- diff(log(df$price))
 adf.test(diff_log_series) # stationary
-df$dividend_yield <- c(NA, diff(log(df$dividend_yield)))
+df$price <- c(NA, diff(log(df$price)))
 
 
 # earnings price ratio
@@ -38,8 +38,31 @@ df$book_market <- c(NA, diff(log(df$book_market)))
 # b/m x-sect factor
 adf.test(df$fbm, alternative = "stationary") # stationary
 
-#data transformation for index variables
-index_vars <- c(price, ret, retx, pce, cay, svar, impvar, vrp, tchi, rsvix)
+#CAPE - not stationary
+adf.test(df$CAPE, alternative = "stationary")
+diff_log_series <- diff(log(df$CAPE))
+adf.test(diff_log_series) # stationary
+df$CAPE <- c(NA, diff(log(df$CAPE)))
+
+adf.test(df$TR_CAPE, alternative = "stationary")
+diff_log_series <- diff(log(df$TR_CAPE))
+adf.test(diff_log_series) # stationary
+df$TR_CAPE <- c(NA, diff(log(df$TR_CAPE)))
+
+adf.test(df$excess_CAPE_yield, alternative = 'stationary')
+diff_log_series <- diff(log(df$excess_CAPE_yield))
+df$excess_CAPE_yield <- c(NA, diff(log(df$excess_CAPE_yield)))
+
+#data transformation for index variables that only requires first differencing
+index_vars_firstdiff <- c("ret", "retx", "pce", "cay", "svar", "impvar", "vrp", "tchi", "rsvix", 
+                "AAA", "BAA", "dividend_yield")
+
+for (x in index_vars_firstdiff) {
+  # Check if the column has already been differenced by ensuring only one leading NA
+  if (sum(is.na(df[[x]])) <= 1) {
+    df[[x]] <- c(NA, diff(df[[x]]))
+  }
+}
 
 # update data
 saveRDS(df, file = "data/complete_data_df.RDS")
