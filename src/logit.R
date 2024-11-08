3465d1b3506256620246d6ca9b16eda71ba03726
yangshutingg <e0773572@u.nus.edu>
2024-11-08 23:03:52 +0800

    remove last 6 rows from test set

--- src/logit.R
+++ src/logit.R
@@ -22,8 +22,8 @@ predicted_probs_logit_h6 <- list()
 for (start in seq(1, nrow(data), by = 10)) {
   
   # Define rolling window train and test data
-  train_data <- data[start:(start + train_window_size + n_validation - 1), ]
-  test_data <- data[(start + train_window_size + n_validation):(start + train_window_size + n_validation + n_test - 1), ]
+  train_data <- data[start:(start + train_window_size + n_validation - 1 - 6), ]
+  test_data <- data[(start + train_window_size + n_validation - 6):(start + train_window_size + n_validation + n_test - 1 - 6), ]
   
   # Logistic regression models for h1, h3, and h6
   logit_h1 <- glm(market_state ~ lag1_tms + lag1_ret + lag1_infl + lag1_lty, data = train_data, family = binomial(link = "logit"))
