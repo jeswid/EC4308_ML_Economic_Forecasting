0808f74b18e8071774ca084ca4937a8eb54697a2
kellywang <wangtingyukelly@gmail.com>
2024-11-10 17:57:21 +0800

    cleaned

--- src/boosting evaluation.R
+++ src/boosting evaluation.R
@@ -38,27 +38,3 @@ for (i in 2:4) {
   print(f1_score)
 }
 
-
-gbm_data = gbm_predict %>%
-  left_join(data, by = c("test_date" = "DATE")) %>%
-  rename(predicted_prob_boosting_h1 = "1-step ahead forecast")
-
-gbm_data$strategy_return_boosting_gbm <- ifelse(
-  gbm_data$predicted_prob_boosting_h1 > 0.5,  # Threshold of 0.5 for investment in stocks
-  gbm_data$ret,          # Invest in stocks: Use the stock return
-  gbm_data$tbl         # Invest in risk-free asset: Use T-bill rate
-)
-
-gbm_data$cum_strategy_return_boosting_gbm <- cumprod(1 + gbm_data$strategy_return_boosting_gbm) - 1
-
-
-data$strategy_return_boosting_gbm_sample_mean <- ifelse(
-  data$predicted_prob_boosting_sample_mean_h1 > 0.5,  # Threshold of 0.5 for investment in stocks
-  data$ret,          # Invest in stocks: Use the stock return
-  data$tbl         # Invest in risk-free asset: Use T-bill rate
-)
-data$strategy_return_boosting_xgb <- ifelse(
-  data$predicted_prob_boosting_xgb_h1 > 0.5,  # Threshold of 0.5 for investment in stocks
-  data$ret,          # Invest in stocks: Use the stock return
-  data$tbl         # Invest in risk-free asset: Use T-bill rate
-)
