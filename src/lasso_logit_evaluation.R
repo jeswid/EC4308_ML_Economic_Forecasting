a6a3f336524327efaa31440f6a88d97b97e3bc99
jjonc <75429917+jjonc@users.noreply.github.com>
2024-11-11 22:00:33 +0800

    Update lasso_logit_evaluation.R

--- src/lasso_logit_evaluation.R
+++ src/lasso_logit_evaluation.R
@@ -4,8 +4,8 @@ library(caret)
 rm(list = ls())
 
 eval_model <- function(n) {
-  data <- readRDS("lasso_logit_predictions.rds")
-  data2 <- readRDS("final_cleaned_data_with_bull_bear.RDS")
+  data <- readRDS("data/lasso_logit_predictions.rds")
+  data2 <- readRDS("data/final_cleaned_data_with_bull_bear.RDS")
   data <- inner_join(data, data2, by = c("Date" = "DATE"))
   data$pred_binary <- ifelse(data[[n]] > 0.5, 0, 1)
   conmat <- confusionMatrix(data = as.factor(data$pred_binary), reference = as.factor(data$market_state), positive = "1", mode = "everything")
