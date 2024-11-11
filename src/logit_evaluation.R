28a5349e66dcb4fb265b72f70c4b8d62915395b3
jjonc <75429917+jjonc@users.noreply.github.com>
2024-11-11 23:12:13 +0800

    delete redundant code logit_evaluation.R

--- src/logit_evaluation.R
+++ src/logit_evaluation.R
@@ -13,34 +13,6 @@ eval_model <- function(n) {
   data <- inner_join(data, data2, by = c("date" = "DATE"))
   data <- data %>% filter(!is.na(.data[[n]]))
   
-  data$strategy_return <- ifelse(
-    data[[n]] > 0.5,  # Threshold of 0.5 for investment in stocks
-    data$original_ret,          # Invest in stocks: Use the stock return
-    data$original_tbl         # Invest in risk-free asset: Use T-bill rate
-  )
-  
-  data$cum_strategy_return <- cumprod(1 + data$strategy_return) - 1
-  
-  # Optional: Use sample average threshold instead of 50%
-  data_full = readRDS("data/final_cleaned_data_with_bull_bear.rds")
-  data_train = data_full[17:438,]
-  sample_avg_threshold <- 1- mean(data_train$market_state)
-  
-  data$strategy_return_avg <- ifelse(
-    data[[n]] > sample_avg_threshold,
-    data$original_ret,
-    data$original_tbl
-  )
-  
-  data$cum_strategy_return_avg <- cumprod(1 + data$strategy_return_avg) - 1
-  
-  # Print summary statistics of the strategies
-  print(summary(data$cum_strategy_return))
-  print(summary(data$cum_strategy_return_avg))
-  
-  strat1_return <- data[nrow(data), "cum_strategy_return"]
-  strat2_return <- data[nrow(data), "cum_strategy_return_avg"]
-  
   # Ensure factor levels are aligned
   data$pred_binary <- factor(ifelse(data[[n]] < 0.5, 0, 1), levels = c(0, 1))
   data$market_state <- factor(data$market_state, levels = c(0, 1))
