f2d92f2b9b2aabc118812990e544b4010fd0bafd
yangshutingg <e0773572@u.nus.edu>
2024-11-12 17:56:08 +0800

    14 PCs to reach 80%

--- src/pca_proof.R
+++ src/pca_proof.R
@@ -1,4 +1,4 @@
-check = readRDS("final_cleaned_data.RDS")
+check = readRDS("data/complete_data_df_transformed.RDS")
 # standardize the data
 df_temp = na.omit(check)
 # Convert factor or character columns to numeric (if appropriate)
@@ -10,4 +10,4 @@ numeric_df <- df_temp[, sapply(df_temp, is.numeric)]  # Remove the non-numeric c
 pca_result <- prcomp(numeric_df, center = TRUE, scale. = TRUE)
 summary(pca_result)  
 
-# because it takes 18 PCs to explain 80% of total variance, we omitted PCA in this analysis
+# because it takes 14 PCs to explain 80% of total variance, we omitted PCA in this analysis
